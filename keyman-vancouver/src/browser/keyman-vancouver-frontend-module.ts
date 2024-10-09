f17cdbbaac15655f11bb6481c196b0078a40f811
Steven R. Loomis <srl295@gmail.com>
2024-10-09 14:36:52 -0500

    try to add getting startef

--- keyman-vancouver/src/browser/keyman-vancouver-frontend-module.ts
+++ keyman-vancouver/src/browser/keyman-vancouver-frontend-module.ts
@@ -6,8 +6,7 @@ import { CommandContribution, MenuContribution } from '@theia/core/lib/common';
 import { ContainerModule } from '@theia/core/shared/inversify';
 import { KeymanVancouverGettingStarted } from './keyman-vancouver-widget';
 import { WidgetFactory } from '@theia/core/lib/browser';
-
-
+import { GettingStartedWidget } from '@theia/getting-started/lib/browser/getting-started-widget';
 
 export default new ContainerModule(bind => {
     // add your contribution bindings here
@@ -16,7 +15,9 @@ export default new ContainerModule(bind => {
 
     bind(KeymanVancouverGettingStarted).toSelf();
     bind(WidgetFactory).toDynamicValue(ctx => ({
-        id: KeymanVancouverGettingStarted.ID,
+        id: GettingStartedWidget.ID,
         createWidget: () => ctx.container.get<KeymanVancouverGettingStarted>(KeymanVancouverGettingStarted)
     })).inSingletonScope();
+
+
 });
